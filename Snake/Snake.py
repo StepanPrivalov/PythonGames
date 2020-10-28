5c587e214d0068b133c4218173080a05631a1dbf
Stepan Privalov <stepan.privalov04@gmail.com>
2020-10-28 23:45:38 +0300

    Snake movement adjusted

--- Snake/Snake.py
+++ Snake/Snake.py
@@ -7,7 +7,7 @@ class cube(object):
 	rows = 20
 	w = 500
 
-	def init(self, start, dirnx = 1, dirny = 0, color = (255, 0, 0)):
+	def __init__(self, start, dirnx = 1, dirny = 0, color = (255, 0, 0)):
 		self.pos = start
 		self.dirnx = 1
 		self.dirny = 0
@@ -17,21 +17,27 @@ class cube(object):
 	def move(self, dirnx, dirny):
 		self.dirnx = dirnx
 		self.dirny = dirny
-		self.pos(self.pos[0] + self.dirnx, self.pos[1] + self.dirny)
+		self.pos = (self.pos[0] + self.dirnx, self.pos[1] + self.dirny)
 
 	def draw(self, surface, eyes = False):
 		dis = self.w // self.rows
 		i = self.pos[0]
 		j = self.pos[1]
 
-		pygame.draw.rect(surface, self.colour, (i * dis + 1, j * dis + 1, dis - 2, dis - 2))
-
+		pygame.draw.rect(surface, self.color, (i * dis + 1, j * dis + 1, dis - 2, dis - 2))
+		if eyes:
+			centre = dis // 2
+			radius = 3
+			circleMiddle = (i * dis + centre - radius, j * dis + 8)
+			circleMiddle2 = (i * dis + dis - radius * 2, j * dis + 8)
+			pygame.draw.circle(surface, (0, 0, 0), circleMiddle, radius)
+			pygame.draw.circle(surface, (0, 0, 0), circleMiddle2, radius)
 
 class snake(object):
 	body = []
 	turns = {}
 
-	def init(self, color, pos):
+	def __init__(self, color, pos):
 		self.color = color
 		self.head = cube(pos)
 		self.body.append(self.head)
@@ -39,9 +45,7 @@ class snake(object):
 		self.dirny = 1
 
 	def move(self):
-		for event in pygame.event.get():
-			if event.type == pygame.QUIT:
-				pygame.quit()
+		
 
 			keys = pygame.key.get_pressed()
 
@@ -68,14 +72,27 @@ class snake(object):
 
 			for i, c in enumerate(self.body):
 				p = c.pos[:]
-				if p in self.turns[p]:
+				if p in self.turns:
 					turn = self.turns[p]
 					c.move(turn[0], turn[1])
 					if i == len(self.body) - 1:
 						self.turns.pop(p)
+				else:
+					if c.dirnx == -1 and c.pos[0] <= 0: c.pos = (c.rows - 1, c.pos[1])
+					elif c.dirnx == 1 and c.pos[0] >= c.rows - 1: c.pos = (0, c.pos[1])
+					elif c.dirny == 1 and c.pos[1] >= c.rows - 1: c.pos = (c.pos[0], 0)
+					elif c.dirny == -1 and c.pos[1] <= 0: c.pos = (c.pos[0], c.rows - 1)
+					else: c.move(c.dirnx, c.dirny)
 
 	def reset(self):
 		pass
+
+	def draw(self, surface):
+		for i, c in enumerate(self.body):
+			if i == 0:
+				c.draw(surface, True)
+			else:
+				c.draw(surface)
 
 def drawGrid(w, rows, surface):
 	sizeBtwn = w // rows
@@ -100,18 +117,21 @@ def redrawWindow(surface):
 
 
 def main():
-	global width, rows
+	global width, rows, s
 	width = 500
 	rows = 20
 	win = pygame.display.set_mode((width, width))
-	#s = snake((255, 0, 0),(10, 10))
+	s = snake((255, 0, 0), (10, 10))
 	flag = True
 	clock = pygame.time.Clock()
 
 	while flag:
+		for event in pygame.event.get():
+			if event.type == pygame.QUIT:
+				pygame.quit()
 		pygame.time.delay(50)
 		clock.tick(10)
-
+		s.move()
 		redrawWindow(win)
 
 main()
