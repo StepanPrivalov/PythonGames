f7094742e5e9a9c33c292d2feefd6a9c2788a601
Stepan Privalov <stepan.privalov04@gmail.com>
2020-11-04 13:16:49 +0300

    Second Ball added

--- Ping-Pong/Ping_Pong.py
+++ Ping-Pong/Ping_Pong.py
@@ -8,8 +8,8 @@ wn.bgcolor("black")
 wn.setup(width = 800, height = 600)
 wn.tracer(0)
 
-score1 = 0
-score2 = 0
+global score1, score2
+score2, score1 = 0, 0
 
 paddle1 = turtle.Turtle()
 paddle1.speed(0)
@@ -36,7 +36,16 @@ ball.color("white")
 ball.penup()
 ball.goto(0, 0)
 ball.dx = 1
-ball.dy = 1
+ball.dy = 1.2
+
+ball2 = turtle.Turtle()
+ball2.speed(0)
+ball2.shape("square")
+ball2.color("red")
+ball2.penup()
+ball2.goto(-50, -50)
+ball2.dx = 1.2
+ball2.dy = 1
 
 pen = turtle.Turtle()
 pen.speed(0)
@@ -67,6 +76,53 @@ def paddle2_down():
     y -= 20
     paddle2.sety(y)
 
+def yUp(b):
+    if b.ycor() > 290:
+        b.sety(290)
+        b.dy *= -1
+        winsound.PlaySound("ping.wav", winsound.SND_ASYNC)
+
+def yDown(b):
+    if b.ycor() < -290:
+        b.sety(-290)
+        b.dy *= -1
+        winsound.PlaySound("ping.wav", winsound.SND_ASYNC)
+
+def xLeft(b):
+    global score1, score2
+    if b.xcor() > 390:
+        b.goto(0, 0)
+        b.dx *= -1
+        score1 += 1
+        b.dx += 0.05
+        b.dy += 0.05
+        pen.clear()
+        pen.write("Player A:{}     Player B:{}".format(score1, score2), align = "center", font = ("Bell MT", 24, "normal"))
+
+def xRight(b):
+    global score1, score2
+    if b.xcor() < -390:
+        b.goto(0, 0)
+        b.dx *= -1
+        b.dx += 0.1
+        b.dy += 0.1
+        score2 += 1
+        pen.clear()
+        pen.write("Player A:{}     Player B:{}".format(score1, score2), align = "center", font = ("Bell MT", 24, "normal"))
+
+def colLeft(b, pad):
+    if (b.xcor() < -340 and b.xcor() > -350) and (b.ycor() < pad.ycor() + 40 and b.ycor() > pad.ycor() - 40):
+        b.setx(-340)
+        b.dx *= -1
+        winsound.PlaySound("ping.wav", winsound.SND_ASYNC)
+
+
+def colRight(b, pad):
+    if (b.xcor() > 340 and b.xcor() < 350) and (b.ycor() < pad.ycor() + 40 and b.ycor() > pad.ycor() - 40):
+        b.setx(340)
+        b.dx *= -1
+        winsound.PlaySound("ping.wav", winsound.SND_ASYNC)
+
 wn.listen()
 wn.onkeypress(paddle1_up, "w")
 wn.onkeypress(paddle1_down, "s")
@@ -74,44 +130,24 @@ wn.onkeypress(paddle2_up, "Up")
 wn.onkeypress(paddle2_down, "Down")
 
 while 1:
-
-    ball.setx(ball.xcor() + ball.dx)
     wn.update()
+    ball.setx(ball.xcor() + ball.dx)
     ball.sety(ball.ycor() + ball.dy)
-   
+    ball2.setx(ball2.xcor() + ball2.dx)
+    ball2.sety(ball2.ycor() + ball2.dy)   
 
+    yUp(ball)
+    yUp(ball2)
+    yDown(ball)
+    yDown(ball2)
 
-    if ball.ycor() > 290:
-        ball.sety(290)
-        ball.dy *= -1
-        winsound.PlaySound("ping.wav", winsound.SND_ASYNC)
-      
+    xLeft(ball2)
+    xRight(ball2)
 
-    if ball.ycor() < -290:
-        ball.sety(-290)
-        ball.dy *= -1
-        winsound.PlaySound("ping.wav", winsound.SND_ASYNC)
+    xLeft(ball)
+    xRight(ball)    
 
-    if ball.xcor() > 390:
-        ball.goto(0, 0)
-        ball.dx *= -1
-        score1 += 1
-        pen.clear()
-        pen.write("Player A:{}     Player B:{}".format(score1, score2), align = "center", font = ("Bell MT", 24, "normal"))
-
-    if ball.xcor() < -390:
-        ball.goto(0, 0)
-        ball.dx *= -1
-        score2 += 1
-        pen.clear()
-        pen.write("Player A:{}     Player B:{}".format(score1, score2), align = "center", font = ("Bell MT", 24, "normal"))
-
-    if (ball.xcor() > 340 and ball.xcor() < 350) and (ball.ycor() < paddle2.ycor() + 40 and ball.ycor() > paddle2.ycor() - 40):
-        ball.setx(340)
-        ball.dx *= -1
-        winsound.PlaySound("ping.wav", winsound.SND_ASYNC)
-
-    if (ball.xcor() < -340 and ball.xcor() > -350) and (ball.ycor() < paddle1.ycor() + 40 and ball.ycor() > paddle1.ycor() - 40):
-        ball.setx(-340)
-        ball.dx *= -1
-        winsound.PlaySound("ping.wav", winsound.SND_ASYNC)
+    colRight(ball, paddle2)
+    colRight(ball2, paddle2)
+    colLeft(ball, paddle1)
+    colLeft(ball2, paddle1)
